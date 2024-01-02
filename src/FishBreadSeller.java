e5e16d483ca1f0048c929797f1da5718d0eea678
taewoocode <qkrxodn6035@gmail.com>
2024-01-02 23:51:56 +0900

    add FishBreadSystem

--- src/FishBreadSeller.java
+++ src/FishBreadSeller.java
@@ -17,15 +17,18 @@ public class FishBreadSeller {
         return availableFishBreads;
     }
 
-    public void sellFishBread(FishBread fishBread, int count){
+    public void sellFishBread(FishBread fishBread, int count) {
         int totalPrice = fishBread.getPrice() * count;
-        if(count > 0 && count <= availableFishBreads.size() && totalPrice <= safe.getMoney()){
-            System.out.println("구매완료: " + count + "개의" + fishBread.getFlavor() + "( " + totalPrice + "원");
-            safe.deductMoney(totalPrice);
-        }else {
-            System.out.println("구매 실패: 잘못된 주문 또는 잔액이 부족");
+        if (count > 0 && count <= availableFishBreads.size() && totalPrice <= safe.getMoney()) {
+            System.out.println( "구매완료: " + count + "개의" + fishBread.getFlavor() + "( " + totalPrice + "원" );
+            safe.deductMoney( totalPrice );
+        } else {
+            System.out.println( "구매 실패: 잘못된 주문 또는 잔액이 부족" );
         }
+    }
 
+    public Safe getSafe() {
+        return safe;
     }
 }
 
--- src/SaleService.java
+++ src/SaleService.java
@@ -1,9 +1,41 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class SaleService {
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
+    private FishBreadSeller fishBreadSeller;
+    private Customer customer;
+
+    public SaleService(FishBreadSeller fishBreadSeller, Customer customer) {
+        this.fishBreadSeller = fishBreadSeller;
+        this.customer = customer;
+    }
+
+    public void sellFishBread(int fishBreadIndex, int count) {
+        if (fishBreadIndex >= 0 && fishBreadIndex < fishBreadSeller.getAvailableFishBreads().size()) {
+            FishBread selectedFishBread = fishBreadSeller.getAvailableFishBreads().get(fishBreadIndex);
+            fishBreadSeller.sellFishBread(selectedFishBread, count, customer);
+        } else {
+            System.out.println("잘못된 붕어빵 번호");
+        }
+    }
+
+    public void displayAvailableFishBreads() {
+        System.out.println("판매 중인 붕어빵 목록과 가격:");
+        ArrayList<FishBread> sellingFishBreads = fishBreadSeller.getAvailableFishBreads();
+        for (int i = 0; i < sellingFishBreads.size(); i++) {
+            FishBread fishBread = sellingFishBreads.get(i);
+            System.out.println(i + 1 + ". " + fishBread.getFlavor() + " - " + fishBread.getPrice() + "원");
+        }
+    }
+
+    public void displayCustomerMoney() {
+        System.out.println("나의 소지금 확인: " + customer.getMoney() + "원");
+    }
+
+    public void displaySafeMoney() {
+        System.out.println("붕어빵 장수의 금고 돈 확인: " + fishBreadSeller.getSafe().getMoney() + "원");
     }
 }
+
